a2193c52c880ba1b98252a7a2196b52fc07f87f4
Sid <dev.mogyahidansid@gmail.com>
2024-08-31 10:57:09 +0800

    Transfer existing screens to pages directory

--- routes/web.php
+++ routes/web.php
@@ -4,12 +4,18 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
+Route::middleware(['auth'])->group(function () {
 
-Route::view('profile', 'profile')
-    ->middleware(['auth'])
-    ->name('profile');
+    Route::view('dashboard', 'pages.dashboard')
+        ->middleware(['verified'])
+        ->name('dashboard');
+
+    Route::view('profile', 'pages.profile')
+        ->name('profile');
+
+    Route::view('permissions', 'pages.permissions')
+        ->name('permission');
+});
+
 
 require __DIR__.'/auth.php';
